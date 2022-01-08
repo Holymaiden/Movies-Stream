ec6e82f52aaf72fd2f9f55fee406e31f784d038b
Fihaa <haikalwatampone3@gmail.com>
2022-01-08 18:42:39 +0800

    Added Image Page

--- Admin/Images.php
+++ Admin/Images.php
@@ -1,6 +1,6 @@
-<?php $title = "Home | Kamar";
+<?php $title = "Home | Images";
 require_once("./Templates/header.php");
-$kamars = many("SELECT * FROM `tbl_kamar`");
+$images = many("SELECT `images`.*,`movies`.`name` FROM `images` LEFT JOIN `movies` ON `images`.`movie_id`=`movies`.`id`");
 ?>
 
 <!-- Content -->
@@ -8,10 +8,10 @@ $kamars = many("SELECT * FROM `tbl_kamar`");
 
         <div class="page-header d-md-flex justify-content-between">
                 <div>
-                        <h3>Kamar</h3>
+                        <h3>Images</h3>
                 </div>
                 <div class="mt-2 mt-md-0">
-                        <a href="kamar_tambah.php"><button type="button" class="btn btn-primary">
+                        <a href="images_tambah.php"><button type="button" class="btn btn-primary">
                                         Baru
                                 </button></a>
                 </div>
@@ -33,34 +33,32 @@ $kamars = many("SELECT * FROM `tbl_kamar`");
                                                                                 </div>
                                                                         </th>
                                                                         <th>ID</th>
+                                                                        <th>Movies</th>
                                                                         <th>Gambar</th>
-                                                                        <th>No Kamar</th>
-                                                                        <th>Jenis</th>
-                                                                        <th>Harga</th>
+                                                                        <th>Alt</th>
                                                                         <th class="text-right">Action</th>
                                                                 </tr>
                                                         </thead>
                                                         <tbody>
-                                                                <?php foreach ($kamars as $i => $v) : ?>
+                                                                <?php foreach ($images as $i => $v) : ?>
                                                                         <tr>
                                                                                 <td></td>
                                                                                 <td><?= ++$i ?></td>
+                                                                                <td><?= $v['name'] ?></td>
                                                                                 <td>
                                                                                         <figure class="avatar avatar-lg mr-2">
-                                                                                                <img src="image/<?= $v['image'] ?>" class="rounded" alt="avatar">
+                                                                                                <img src="../images/<?= $v['source'] ?>" class="rounded" alt="avatar">
                                                                                         </figure>
                                                                                 </td>
-                                                                                <td><?= $v['no_kamar'] ?></td>
-                                                                                <td><?= $v['jenis'] ?></td>
-                                                                                <td><?= $v['harga'] ?></td>
+                                                                                <td><?= $v['alt'] ?></td>
                                                                                 <td class="text-right">
                                                                                         <div class="dropdown">
                                                                                                 <a href="#" data-toggle="dropdown" class="btn btn-floating" aria-haspopup="true" aria-expanded="false">
                                                                                                         <i class="ti-more-alt"></i>
                                                                                                 </a>
                                                                                                 <div class="dropdown-menu dropdown-menu-right">
-                                                                                                        <a href="kamar_edit.php?v=<?= $v['id'] ?>" class="dropdown-item">Edit</a>
-                                                                                                        <a href="kamar_hapus.php?v=<?= $v['id'] ?>" class="dropdown-item text-danger">Delete</a>
+                                                                                                        <a href="images_edit.php?v=<?= $v['id'] ?>" class="dropdown-item">Edit</a>
+                                                                                                        <a href="images_hapus.php?v=<?= $v['id'] ?>" class="dropdown-item text-danger">Delete</a>
                                                                                                 </div>
                                                                                         </div>
                                                                                 </td>
